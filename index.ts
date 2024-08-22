ab53e632e61e342c961e5b9780be79097234a069
Ext-Ticha.Ub <Ext-Ticha.Ub@Sansiri.com>
2024-08-22 17:21:22 +0700

    [main] commit-8 : add file and edit index.ts

--- index.ts
+++ index.ts
@@ -5,8 +5,9 @@ const c_1 = 3; // test edit for merge feature/one => main
 const d = 4;
 const _d = 4;
 const e = 5;
+console.log("add form main")
 
 
 const feature_aa = 1;
-console.log("feature/one 2-3")
+console.log("feature/one 2-3 main")
 console.log("feature/one 2-4")
