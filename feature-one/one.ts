7ea6c85a78403f75ca78772e465fda70b3788825
Ext-Ticha.Ub <Ext-Ticha.Ub@Sansiri.com>
2024-08-23 08:46:47 +0700

    Revert "Merge branch 'feature/one'"
    This reverts commit 202c8ef4091c2d04393fd89f1877a8dc50ef9563, reversing
    changes made to b35abd80f634164313594de3c014487dcad6128d.

--- feature-one/one.ts
+++
@@ -1,2 +0,0 @@
-console.log("commit one")
-console.log("commit one_two_theee")
--- index.ts
+++ index.ts
@@ -1,13 +1,7 @@
 const a = 1;
 const b = 2;
 const c = 3;
-const c_1 = 3; // test edit for merge feature/one => main
+const c_1 = 3;
 const d = 4;
 const _d = 4;
 const e = 5;
-console.log("add form main")
-
-
-const feature_aa = 1;
-console.log("feature/one 2-3 main")
-console.log("feature/one 2-4")
